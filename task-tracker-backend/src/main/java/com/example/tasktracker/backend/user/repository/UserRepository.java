1643f015addec3b9f88ee7ae60180b054bcb34c2
dailar <dailar401@gmail.com>
2025-05-05 18:50:48 +0300

    feat(backend): Implement User Entity and UserRepository

---
+++ task-tracker-backend/src/main/java/com/example/tasktracker/backend/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.tasktracker.backend.user.repository;
+
+import com.example.tasktracker.backend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}

77e83c6219db7caed102182d23a1c61b0a0fc1b4
dailar <dailar401@gmail.com>
2025-05-08 19:01:19 +0300

    test(backend-persistence): Add initial integration tests using Spring Boot Testcontainers

--- task-tracker-backend/src/test/java/com/example/tasktracker/backend/TaskTrackerBackendApplicationTests.java
+++ task-tracker-backend/src/test/java/com/example/tasktracker/backend/TaskTrackerBackendApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example.tasktracker.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude =
+		{DataSourceAutoConfiguration.class, LiquibaseAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class TaskTrackerBackendApplicationTests {
 
 	@Test
---
+++ task-tracker-backend/src/test/java/com/example/tasktracker/backend/user/repository/UserRepositoryIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.example.tasktracker.backend.user.repository;
+
+import com.example.tasktracker.backend.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UserRepositoryIntegrationTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgresContainer=
+            new PostgreSQLContainer<>("postgres:17.4-alpine");
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void whenSaveNewUser_thenSuccess() {
+        User user = new User(null, "test@example.com", "password123", null, null);
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isNotNull();
+        assertThat(savedUser.getEmail()).isEqualTo("test@example.com");
+        assertThat(savedUser.getCreatedAt()).isNotNull();
+        assertThat(savedUser.getUpdatedAt()).isNotNull();
+    }
+}

eb6ba9d73437a33db1df622d701076f8a2e4aa82
dailar <dailar401@gmail.com>
2025-05-14 15:36:31 +0300

    feat(security): Implement basic SecurityConfig

---
+++ task-tracker-backend/src/test/java/com/example/tasktracker/backend/security/config/SecurityConfigIT.java
@@ -0,0 +1,8 @@
+package com.example.tasktracker.backend.security.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO add tests for SecurityConfig after controllers implemented
+class SecurityConfigIT {
+
+}

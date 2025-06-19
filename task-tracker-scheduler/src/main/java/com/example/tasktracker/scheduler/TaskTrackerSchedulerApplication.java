11a68ccac617ff5e7352e9eb0350fccc0b1c64db
dailar <dailar401@gmail.com>
2025-06-19 08:31:08 +0300

    feat(scheduler): Add initial module structure

---
+++ task-tracker-scheduler/src/main/java/com/example/tasktracker/scheduler/TaskTrackerSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktracker.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TaskTrackerSchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerSchedulerApplication.class, args);
+    }
+}

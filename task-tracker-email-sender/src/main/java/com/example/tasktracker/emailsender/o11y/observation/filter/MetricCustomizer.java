162ec30e683147cf3f27f3c8fbc964de807ca26f
dailar <dailar401@gmail.com>
2026-06-02 13:56:29 +0300

    fix(email-sender): resolve executor and SMTP pool telemetry loss

--- task-tracker-email-sender/src/main/java/com/example/tasktracker/emailsender/o11y/observation/filter/MetricCustomizer.java
+++ task-tracker-email-sender/src/main/java/com/example/tasktracker/emailsender/o11y/observation/filter/MetricCustomizer.java
@@ -12,6 +12,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class MetricCustomizer {
+    public static final double[] DEFAULT_OTEL_BUCKET_NANOS =
+            new double[]{
+                    5_000_000.0,
+                    10_000_000.0,
+                    25_000_000.0,
+                    50_000_000.0,
+                    75_000_000.0,
+                    100_000_000.0,
+                    250_000_000.0,
+                    500_000_000.0,
+                    750_000_000.0,
+                    1_000_000_000.0,
+                    2_500_000_000.0,
+                    5_000_000_000.0,
+                    7_500_000_000.0,
+                    10_000_000_000.0
+            };
+
     private final ChunkRateLimitConvention chunkRateLimitConvention;
 
     @Bean
@@ -20,6 +38,15 @@ public class MetricCustomizer {
             @Override
             public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
                 String name = id.getName();
+
+                if (id.getName().equals("resilience4j.circuitbreaker.calls")) {
+                    return DistributionStatisticConfig.builder()
+                            .percentilesHistogram(false)
+                            .serviceLevelObjectives(DEFAULT_OTEL_BUCKET_NANOS)
+                            .build()
+                            .merge(config);
+                }
+
                 if (name.equals(chunkRateLimitConvention.getName())) {
                     return DistributionStatisticConfig.builder()
                             .percentilesHistogram(false)
@@ -32,6 +59,12 @@ public class MetricCustomizer {
                     return DistributionStatisticConfig.builder()
                             .percentilesHistogram(false)
                             .serviceLevelObjectives(BaseKafkaMessagingConvention.SLO_BUCKETS)
+                            .build()
+                            .merge(config);
+                }
+                if (name.equals("jvm.gc.pause")) {
+                    return DistributionStatisticConfig.builder()
+                            .serviceLevelObjectives(DEFAULT_OTEL_BUCKET_NANOS)
                             .build()
                             .merge(config);
                 }
